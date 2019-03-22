5509bdc36f26013647fd7b405a61dda46076080c
Daniel Killoran <dankilloran@gmail.com>
2019-03-22 02:38:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ var destructivelyUpdateObjectWithKeyAndValue = function(recipes, Cookie, flour) 
 }
 
 var deleteFromObjectByKey = function(recipes, Pizza) {
-  Object.assign({}, recipes, { [Pizza]: cheese })
+  Object.assign({}, delete recipes.Pizza)
 }
